5e91e749b17509f6136efbbd5e8a5f7c10c9f450
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-02-17 18:41:49 +0530

    Share link feature added

--- src/components/ShareContentModal.tsx
+++ src/components/ShareContentModal.tsx
@@ -1,6 +1,8 @@
 import Modal from './Modal';
 import Button from './Button';
 import { LuCopy } from "react-icons/lu";
+import axios from 'axios';
+import { useRef } from 'react';
 
 interface ShareContentModalInterface{
     open: boolean,
@@ -11,13 +13,51 @@ export default function ShareContentModal({
     open, 
     setOpen
 }: ShareContentModalInterface){
+    const URL = 'http://localhost:6001/api/v1/brain/share';
+    const alerDivtRef= useRef<HTMLInputElement | null>(null);
+
+    const handleShareContent = () => {
+        const token = localStorage.getItem("token")
+
+        axios.post(URL, {
+            share: true
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((res) => {
+            if(res.data.success){
+                if(alerDivtRef.current){
+                    alerDivtRef.current.innerText = "Copied share link to your clipboard."
+                    setTimeout(() => {
+                        if (alerDivtRef.current){
+                            alerDivtRef.current.innerText = ""
+                        }
+                        setOpen(false)
+                    }, 2000)
+                }
+                navigator.clipboard.writeText(`${window.location.href}/api/v1/brain/${res.data.data.link}`);
+            }
+        }).catch(() => {
+            if(alerDivtRef.current){
+                alerDivtRef.current.innerText = "Some error occured. Please try again."
+                setTimeout(() => {
+                    if (alerDivtRef.current){
+                        alerDivtRef.current.innerText = ""
+                    }
+                }, 1500)
+            }
+        })
+    }
+
     return (
         <Modal open={open} setOpen={setOpen} title='Share Your Second Brain' >
             <div className='p-2 w-full flex flex-col gap-2'>
                 <p className='mt-5 text-gray-500 mb-3'>
                     Share your entire collection of notes, documents, tweets, and videos with others. They'll be able to import your content into their own Second Brain.
                 </p>
-                <Button variant='primary' text='Share Brain' size="lg" startIcon={<LuCopy />} />
+                <div className="text-center text-gray-400 text-sm" ref={alerDivtRef} > </div>
+                <Button variant='primary' text='Share Brain' size="lg" startIcon={<LuCopy />} onClick={handleShareContent} />
                 <p className='text-gray-400 text-center'>5 items will be shared</p>
             </div>
         </Modal>
